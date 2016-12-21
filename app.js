d4895adb2aebd0a7ec30bbf5d02dda42b9ead601
wolyshaw <shaw@xwlong.com>
2016-12-21 19:39:31 +0800

    add

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const config = require('./config/config')
+const bodyParser = require('body-parser')
+const commonRoute = require('./router/common/token')
 const getRoute = require('./router/get')
 const app = express()
 let port = config.port || 3000
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', commonRoute)
 app.use('/get', getRoute)
 
 app.listen(port, function(){
---
+++ router/common/token.js
@@ -0,0 +1,40 @@
+const qiniu = require('qiniu')
+const express = require('express')
+const config = require('../../config/config')
+
+qiniu.conf.ACCESS_KEY = config.qiniu.AccessKey
+qiniu.conf.SECRET_KEY = config.qiniu.SecretKey
+
+let router = express.Router()
+
+router.post('/token', function(req, res, next){
+  let data = {}
+  let uptoken = function(bucket, key) {
+    var putPolicy = new qiniu.rs.PutPolicy(bucket + ':' + key);
+    return putPolicy.token();
+  }
+  if (!req.body.name) {
+    data.msg = '请填写图片名称'
+    res.json(data)
+  }
+  if (!req.body.path) {
+    data.msg = '请选择路径'
+    res.json(data)
+  }
+
+  let bucket = config.qiniu.bucket
+
+  let key = req.body.path + '/' + req.body.name
+
+  data = {
+    msg: 'token获取成功',
+    code: 200,
+    data: {
+      token: uptoken(bucket, key)
+    }
+  }
+  res.header('Access-Control-Allow-Origin', '*')
+  res.json(data)
+})
+
+module.exports = router
